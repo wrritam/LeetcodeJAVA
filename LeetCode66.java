66ca37b32bd2daac457aa9b813f1fca46c619558
Ritam Samanta <91908473+RitamSamant@users.noreply.github.com>
2022-04-01 20:49:25 +0530

    Update LeetCode66.java

--- LeetCode66.java
+++ LeetCode66.java
@@ -1,23 +1,19 @@
-
 class Solution {
-    public int[] plusOne(int[] digits) {        
-
-    int carry = 0;
-    
-    for(int i = digits.length - 1; i >= 0; i--){
-        if(digits[i] == 9){
-            digits[i] = 0;
-            carry = 1;
+    public int[] plusOne(int[] digits) {
+     
+        
+        int x = digits.length;
+        for(int i = x-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+                
+            }
+            digits[i]=0;
         }
-        else{
-            digits[i]++;
-            return digits;
-        }
-    }
-    if(carry == 1){
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
-    }
-    return digits;
+        
+        int[] newArray = new int[x+1];
+        newArray[0]=1;
+        return newArray;
     }
 }
