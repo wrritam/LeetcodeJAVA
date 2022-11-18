f9c5f3ad957910978ffef6c2dd1bd87548a55607
Ritam Samanta <91908473+RitamSamant@users.noreply.github.com>
2022-11-18 19:24:05 +0530

    Create LeetCode263.java

---
+++ LeetCode - Java/LeetCode263.java
@@ -0,0 +1,11 @@
+
+ class Solution {
+    public boolean isUgly(int n) {
+        if(n==0)return false;
+        if(n==1)return true;
+        if(n%2==0) return isUgly(n/2);
+        else if(n%3==0) return isUgly(n/3);
+        else   if(n%5==0) return isUgly(n/5);
+        return false;
+    }
+}
