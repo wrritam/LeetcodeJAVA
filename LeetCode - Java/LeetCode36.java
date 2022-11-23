dfc595bfee2228b88747178bf71be88aba2a6074
Ritam Samanta <91908473+RitamSamant@users.noreply.github.com>
2022-11-23 17:19:55 +0530

    Create LeetCode36.java

---
+++ LeetCode - Java/LeetCode36.java
@@ -0,0 +1,33 @@
+
+class Solution {
+
+  public boolean isValidSudoku(char[][] board) {
+    int N = 9;
+    int[][] rows = new int[N][N];
+    int[][] cols = new int[N][N];
+    int[][] boxes = new int[N][N];
+    for (int r = 0; r < N; r++) {
+      for (int c = 0; c < N; c++) {
+        if (board[r][c] == '.') {
+          continue;
+        }
+        int pos = board[r][c] - '1';
+        if (rows[r][pos] == 1) {
+          return false;
+        }
+        rows[r][pos] = 1;
+        if (cols[c][pos] == 1) {
+          return false;
+        }
+        cols[c][pos] = 1;
+        int idx = (r / 3) * 3 + c / 3;
+        if (boxes[idx][pos] == 1) {
+          return false;
+        }
+        boxes[idx][pos] = 1;
+      }
+    }
+    return true;
+  }
+  }
+
